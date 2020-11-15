5a4ddb442f6a386334fe363ff8a7902def18b21d
Leonid Genkin <lgenkin@nvidia.com>
2020-11-15 10:38:50 +0200

    asd

--- src/uct/ugni/udt/a.c
+++ src/uct/ugni/udt/a.c
@@ -225,7 +225,7 @@ uct_ugni_udt_ep_am_common_send(const unsigned is_short, uct_ugni_udt_ep_t *ep, u
 }
 
 ucs_status_t uct_ugni_udt_ep_am_short(uct_ep_h tl_ep, uint8_t id, uint64_t header,
-                                const void *payload, unsigned length)
+                                      const void *payload, unsigned length)
 {
     uct_ugni_udt_iface_t *iface = ucs_derived_of(tl_ep->iface, uct_ugni_udt_iface_t);
     uct_ugni_udt_ep_t *ep = ucs_derived_of(tl_ep, uct_ugni_udt_ep_t);
