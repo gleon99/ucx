69dcebfdbb5d34d5a24eb25a4a461cdbb5417d1c
Leonid Genkin <lgenkin@nvidia.com>
2021-01-28 14:23:05 +0200

    Moved IoDemoRandom; crashes

--- test/apps/iodemo/cuda_util/cuda_util.cu
+++ test/apps/iodemo/cuda_util/cuda_util.cu
@@ -1,8 +1,132 @@
-// int main() { return 1; }
-
 #include <stdio.h>
 #include "cuda_util.h"
 
+
+/**
+ * Linear congruential generator (LCG):
+ * n[i + 1] = (n[i] * A + C) % M
+ * where A, C, M used as in glibc
+ */
+    void IoDemoRandom::srand(unsigned seed) {
+        _seed = seed & _M;
+    }
+
+   /*  template <typename T>
+    T IoDemoRandom::rand(T min, T max) 
+
+    template <typename T>
+    T IoDemoRandom::rand(unsigned &seed, T min, T max)  */
+
+    void IoDemoRandom::fill(unsigned &seed, void *buffer, size_t size) {
+        size_t body_count = size / sizeof(uint64_t);
+        size_t tail_count = size & (sizeof(uint64_t) - 1);
+        uint64_t *body    = reinterpret_cast<uint64_t*>(buffer);
+        uint8_t *tail     = reinterpret_cast<uint8_t*>(body + body_count);
+
+        fill(seed, body, body_count);
+        fill(seed, tail, tail_count);
+    }
+
+    size_t IoDemoRandom::validate(unsigned &seed, const void *buffer,
+                                  size_t size) {
+        size_t body_count    = size / sizeof(uint64_t);
+        size_t tail_count    = size & (sizeof(uint64_t) - 1);
+        const uint64_t *body = reinterpret_cast<const uint64_t*>(buffer);
+        const uint8_t *tail  = reinterpret_cast<const uint8_t*>(body + body_count);
+
+        size_t err_pos = validate(seed, body, body_count);
+        if (err_pos < body_count) {
+            return err_pos * sizeof(body[0]);
+        }
+
+        err_pos = validate(seed, tail, tail_count);
+        if (err_pos < tail_count) {
+            return (body_count * sizeof(body[0])) + (err_pos * sizeof(tail[0]));
+        }
+
+        return size;
+    }
+
+    void IoDemoRandom::setMemoryType(ucs_memory_type_t memory_type)
+    {
+        _memory_type = memory_type;
+    }
+
+    template <typename T>
+    void IoDemoRandom::fill(unsigned &seed, T *buffer, size_t count) {
+#ifdef HAVE_CUDA
+        T temp;
+#endif
+
+        for (size_t i = 0; i < count; ++i) {
+            switch (_memory_type) {
+#ifdef HAVE_CUDA
+            case UCS_MEMORY_TYPE_CUDA:
+                temp = rand<T>(seed);
+                cudaMemcpy(&buffer[i], &temp, sizeof(T), cudaMemcpyDefault);
+                break;
+            case UCS_MEMORY_TYPE_CUDA_MANAGED:
+#endif
+            case UCS_MEMORY_TYPE_HOST:
+                buffer[i] = rand<T>(seed);
+                break;
+            default:
+                /* Unreachable - would fail in ctor */
+                abort();
+            }
+        }
+    }
+
+    template <typename T>
+    size_t IoDemoRandom::validate(unsigned &seed, const T *buffer, size_t count) {
+#ifdef HAVE_CUDA
+        T expected_value, actual_value;
+#endif
+
+        for (size_t i = 0; i < count; ++i) {
+            switch (_memory_type) {
+#ifdef HAVE_CUDA
+            case UCS_MEMORY_TYPE_CUDA:
+                expected_value = rand<T>(seed);
+                cudaMemcpy(&actual_value, &buffer[i], sizeof(T),
+                           cudaMemcpyDefault);
+                if (actual_value != expected_value) {
+                    return i;
+                }
+                break;
+            case UCS_MEMORY_TYPE_CUDA_MANAGED:
+#endif
+            case UCS_MEMORY_TYPE_HOST:
+                if (buffer[i] != rand<T>(seed)) {
+                    return i;
+                }
+                break;
+
+            default:
+                /* Unreachable - would fail in ctor */
+                abort();
+            }
+        }
+
+        return count;
+    }
+
+//     static       unsigned     _seed;
+//     static const unsigned     _A;
+//     static const unsigned     _C;
+//     static const unsigned     _M;
+//     static ucs_memory_type_t _memory_type;
+// };
+unsigned IoDemoRandom::_seed    = 0;
+const unsigned IoDemoRandom::_A = 1103515245U;
+const unsigned IoDemoRandom::_C = 12345U;
+const unsigned IoDemoRandom::_M = 0x7fffffffU;
+ucs_memory_type_t IoDemoRandom::_memory_type = UCS_MEMORY_TYPE_HOST;
+
+
+
+const static int cuda_util_blocks_num = 256;
+const static int cuda_util_threads_per_block = 256;
 
 __global__
 void LEO_add(int *x)
@@ -14,7 +138,7 @@ void LEO_add(int *x)
 }
 
 void LEO_add2(int *x)
-    {
-        LEO_add<<<1, 1>>>(x);
-        cudaDeviceSynchronize();
-    }
+{
+    LEO_add<<<cuda_util_blocks_num, cuda_util_threads_per_block>>>(x);
+    cudaDeviceSynchronize();
+}
--- test/apps/iodemo/cuda_util/cuda_util.h
+++ test/apps/iodemo/cuda_util/cuda_util.h
@@ -1,3 +1,61 @@
+
+#include <stddef.h>
+#include <stdint.h>
+#include <ucs/memory/memory_type.h>
+#include <limits>
+
+class IoDemoRandom {
+public:
+    static void srand(unsigned seed);
+  
+    template <typename T>
+    static inline T rand(T min = std::numeric_limits<T>::min(),
+                         T max = std::numeric_limits<T>::max() - 1) {
+        return rand(_seed, min, max);
+    }
+
+    template <typename T>
+    static T rand(unsigned &seed, T min = std::numeric_limits<T>::min(),
+                         T max = std::numeric_limits<T>::max() - 1) {
+        seed = (seed * _A + _C) & _M;
+        /* To resolve that LCG returns alternating even/odd values */
+        if (max - min == 1) {
+            return (seed & 0x100) ? max : min;
+        } else {
+            return T(seed) % (max - min + 1) + min;
+        }
+    }
+
+    static void fill(unsigned &seed, void *buffer, size_t size);
+
+    static size_t validate(unsigned &seed, const void *buffer,
+                                  size_t size);
+
+    static void setMemoryType(ucs_memory_type_t memory_type);
+
+private:
+    template <typename T>
+    static inline void fill(unsigned &seed, T *buffer, size_t count);
+
+    template <typename T>
+    static inline size_t validate(unsigned &seed, const T *buffer, size_t count);
+
+    static       unsigned     _seed;
+    static const unsigned     _A;
+    static const unsigned     _C;
+    static const unsigned     _M;
+    static ucs_memory_type_t _memory_type;
+};
+/* 
+template<>
+uint64_t IoDemoRandom::rand(uint64_t min, uint64_t max);
+
+template<>
+unsigned long IoDemoRandom::rand(unsigned long min, unsigned long max);
+
+template<>
+uint32_t IoDemoRandom::rand(uint32_t min, uint32_t max); */
+
 __global__ 
 void LEO_add(int *x);
 void LEO_add2(int *x);
